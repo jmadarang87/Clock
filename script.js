3d9d722188c574133b4561a7e3e572ac1d3dd79b
jmadarang87 <jmadarang87@gmail.com>
2021-06-18 23:04:24 -0500

    added digital clock

--- script.js
+++ script.js
@@ -1,29 +1,42 @@
 const hour = document.getElementById('hour-hand').style;
 const minute = document.getElementById('minute-hand').style;
 const second = document.getElementById('second-hand').style;
+const digital = document.getElementById('digital');
 
 setInterval(setClock, 1000);
 
 function setClock() {
 
     const date = new Date();
-    let hh = date.getHours() * 30;
-    const mm = date.getMinutes() * 6;
-    const ss = date.getSeconds() * 6;
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
 
-    hh += (mm/6/60*30);
+    const hourDeg = 30;
+    const minuteDeg = 6;
+    const secondDeg = 6;
 
-    minute.setProperty('--mh-degrees', `${mm}deg`);
-    second.setProperty('--sh-degrees', `${ss}deg`);
-    hour.setProperty('--hh-degrees', `${hh}deg`);
+    let setHour = hh * hourDeg;
+    const setMinute = mm * minuteDeg;
+    const setSecond = ss * secondDeg;
 
-    // if ( hh > 12 ) {
-    //     console.log(hh);
-    //     const newHour = (((hh - 12) * 30) + ( mm / 6)) * 6;
-    //     hour.setProperty('--hh-degrees', `${newHour}deg`);
-    //     } else {
-    //     hour.setProperty('--hh-degrees', `${hh}deg`);
-    //     } 
+    setHour += (setMinute/6/60*30);
+
+    minute.setProperty('--mh-degrees', `${setMinute}deg`);
+    second.setProperty('--sh-degrees', `${setSecond}deg`);
+    hour.setProperty('--hh-degrees', `${setHour}deg`);
+
+    if ( hh > 12 ) {
+        hh -= 12;
+    };
+    if ( ss < 10 ) {
+        ss = `0${ss}`;
+    };
+    if ( mm < 10 ) {
+        mm = `0${mm}`;
+    };
+
+    digital.innerHTML = `${hh}:${mm}:${ss} CST`
 
 }
 
